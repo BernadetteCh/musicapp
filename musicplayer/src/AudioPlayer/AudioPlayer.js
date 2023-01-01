dc537b16e163ba512583f23b83339a3126ec1f59
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-01 20:52:55 +0100

    Fetch get request

--- musicplayer/src/AudioPlayer/AudioPlayer.js
+++ musicplayer/src/AudioPlayer/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PlayerPauseIcon from "../PlayPauseIcon";
 import BackwardStepIcon from "../Icons/Backward-step.png";
 import ForwardStepIcon from "../Icons/Forward-step.png";
@@ -7,7 +7,19 @@ import Kaleo from "../Kaleo - I walk on Water.mp3";
 import Queen from "../Bohemian - Rhapsody .mp3";
 
 const AudioPlayer = ({ sameRender, state }) => {
+  const [music, setMusic] = useState();
   const audioElem = useRef();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("http://localhost:8080/api/");
+      const data = await response.json();
+      console.log(data);
+      console.log(Kaleo);
+      setMusic(data[0].title);
+    };
+    getData();
+  }, []);
 
   useEffect(() => {
     if (state) {
@@ -19,10 +31,19 @@ const AudioPlayer = ({ sameRender, state }) => {
   const playPause = () => {
     sameRender(state);
   };
+  console.log(music);
+  console.log(Queen);
   return (
     <div className="audioplayer-container">
       <figure>
-        <audio src={Queen} ref={audioElem}></audio>
+        {/* {music === undefined ? (
+          console.log("no music")
+        ) : (
+          <audio src={music} ref={audioElem}></audio>
+        )} */}
+        <audio src={music} ref={audioElem}>
+          {/* <source src={music} type="audio/mp3" /> */}
+        </audio>
         <img
           src={BackwardStepIcon}
           alt="backward-step-icon"
