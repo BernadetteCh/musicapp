18fea0fae3817472bf57758ec7122f32c6c1a86b
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-21 22:53:00 +0100

    Remove comments

--- musicplayer/src/AudioPlayer/AudioPlayer.js
+++ musicplayer/src/AudioPlayer/AudioPlayer.js
@@ -10,74 +10,36 @@ const getData = async (songId, dataSetter) => {
   const data = await response.json();
   dataSetter(data.song.file);
 };
-const AudioPlayer = ({ sameRender, state, songId }) => {
+const AudioPlayer = ({ sameRender, playorpause, songId }) => {
   console.log(songId);
+  console.log(playorpause);
 
   const [music, setMusic] = useState("");
-
-  //const audioElem = useRef();
+  const [playSong, setPlaySong] = useState();
   const audioElem = useRef();
-  const songElem = useRef(new Audio(music));
-
-  // useEffect(() => {
-  //   console.log("fetchmusic");
-  //   const getData = async () => {
-  //     const response = await fetch(`http://localhost:8080/api/${songId}`);
-  //     const data = await response.json();
-  //     setMusic(data.song.file);
-  //   };
-
-  //   getData();
-  // }, [songId]);
 
   useEffect(() => {
-    console.log(state);
-    console.log(music);
-    const getData = async () => {
-      const response = await fetch(`http://localhost:8080/api/${songId}`);
-      const data = await response.json();
-      setMusic(data.song.file);
-      audioElem.current.play();
-    };
-
-    if (state) {
-      console.log(music);
-      //getData();
-      audioElem.current.play();
-    } else {
-      audioElem.current.pause();
-    }
-  }, [state, songId]);
-
-  if (state == true && music !== "") {
-    audioElem.current.play();
-  }
+    getData(songId, setMusic);
+  }, [songId]);
 
   const playPause = (boolean) => {
-    // console.log(state);
-    //sameRender(state);
-
-    console.log(state);
-    sameRender(state);
+    sameRender(boolean);
   };
-  //playPause();
-
+  // playPause();
+  console.log(playorpause);
   return (
     <div className="audioplayer-container">
-      <figure>
-        <audio src={music} ref={audioElem}></audio>
-        <img
-          src={BackwardStepIcon}
-          alt="backward-step-icon"
-          className="track-back-icon"
-        ></img>
-        {/* <PlayerPauseIcon sameRender={playPause} state={playorpause} /> */}
-        <img
-          src={ForwardStepIcon}
-          className="track-forward-icon"
-          alt="track-forward-icon"
-        ></img>
-      </figure>
+      <img
+        src={BackwardStepIcon}
+        alt="backward-step-icon"
+        className="track-back-icon"
+      ></img>
+      <PlayerPauseIcon sameRender={playPause} state={playorpause} />
+      <img
+        src={ForwardStepIcon}
+        className="track-forward-icon"
+        alt="track-forward-icon"
+      ></img>
     </div>
   );
 };
