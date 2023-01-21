5eb2e6f8dd7bc07ea00925ec58ea7dfc901f983b
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-21 22:52:44 +0100

    Change return statement to be synchron with icon in Musictablerow

--- musicplayer/src/PlayPauseIcon.js
+++ musicplayer/src/PlayPauseIcon.js
@@ -4,33 +4,53 @@ import PlayIcon from "../src/Icons/Play.png";
 import PauseIcon from "../src/Icons/Pause.png";
 
 const PlayPauseIcon = ({ sameRender, state }) => {
-  const [playing, setPlaying] = useState(state);
+  const [playing, setPlaying] = useState(false);
   const playPause = (boolean) => {
-    console.log(state);
     console.log(boolean);
-    setPlaying(boolean, sameRender(boolean));
-    // sameRender(boolean);
+    // setPlaying(boolean, sameRender(boolean));
+    sameRender(boolean);
   };
+  console.log(state);
 
-  if (playing) {
-    return (
-      <img
-        src={PlayIcon}
-        alt="playIcon"
-        className="play-icon"
-        onClick={() => playPause((state) => !state)}
-      ></img>
-    );
-  } else {
+  if (state) {
     return (
       <div>
         <img
           src={PauseIcon}
           className="pause-icon"
-          onClick={() => playPause((state) => !state)}
+          // onClick={() => playPause((state) => !state)}
+          onClick={() => {
+            playPause(!state);
+          }}
           alt="pause-icon"
         ></img>
       </div>
+      // <img
+      //   src={PlayIcon}
+      //   alt="playIcon"
+      //   className="play-icon"
+      //   onClick={() => playPause((state) => !state)}
+      // ></img>
+    );
+  } else {
+    return (
+      <img
+        src={PlayIcon}
+        alt="playIcon"
+        className="play-icon"
+        // onClick={() => playPause((state) => !state)}
+        onClick={() => {
+          playPause(!state);
+        }}
+      ></img>
+      // <div>
+      //   <img
+      //     src={PauseIcon}
+      //     className="pause-icon"
+      //     onClick={() => playPause((state) => !state)}
+      //     alt="pause-icon"
+      //   ></img>
+      // </div>
     );
   }
 };
