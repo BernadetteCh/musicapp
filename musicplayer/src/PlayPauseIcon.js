cb9bb8ff603beff9fd3b56534f357f58f35892f1
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-21 11:40:53 +0100

    Add toggle to onClick function

--- musicplayer/src/PlayPauseIcon.js
+++ musicplayer/src/PlayPauseIcon.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import "../src/AudioPlayer/AudioPlayer.css";
 import PlayIcon from "../src/Icons/Play.png";
 import PauseIcon from "../src/Icons/Pause.png";
+
 const PlayPauseIcon = ({ sameRender, state }) => {
   const [playing, setPlaying] = useState(state);
-  const playPause = () => {
-    sameRender(playing);
+  const playPause = (boolean) => {
+    console.log(state);
+    console.log(boolean);
+    setPlaying(boolean, sameRender(boolean));
+    // sameRender(boolean);
   };
 
-  if (state === false) {
+  if (playing) {
     return (
       <img
         src={PlayIcon}
         alt="playIcon"
         className="play-icon"
-        onClick={playPause}
+        onClick={() => playPause((state) => !state)}
       ></img>
     );
   } else {
@@ -23,7 +27,7 @@ const PlayPauseIcon = ({ sameRender, state }) => {
         <img
           src={PauseIcon}
           className="pause-icon"
-          onClick={playPause}
+          onClick={() => playPause((state) => !state)}
           alt="pause-icon"
         ></img>
       </div>
