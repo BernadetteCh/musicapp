037f000877a3b1c1bfa4d836b420be3ec91008e1
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-21 11:40:06 +0100

    Remove comment and add reqeuest so find song

--- Backend/Router/routes.js
+++ Backend/Router/routes.js
@@ -1,26 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const fileUpload = require("express-fileupload");
+// const fileUpload = require("express-fileupload");
 const Music = require("../Schemas/MusicSchema");
 
 router.get("/", async (req, res) => {
-  //res.setHeader("Content-Type", "audio/mpeg");
   const data = await Music.find({ title: { $exists: true } });
   res.send(data);
 });
-
-// router.post(
-//   "/newSong",
-//   fileUpload({ createParentPath: true }),
-//   async (req, res) => {
-//     console.log("Hallo");
-
-//     console.log(req.body);
-//     console.log(req.files);
-
-//     res.json({ status: "OK" });
-//   }
-// );
 
 router.post("/newSong", async (req, res) => {
   let newMusic = new Music({
@@ -36,4 +22,12 @@ router.post("/newSong", async (req, res) => {
     res.json({ error: e });
   }
 });
+
+router.get("/:songId", async (req, res) => {
+  console.log("Hi");
+  const song = await Music.findById(req.params.songId);
+  res.status(200).json({ song });
+  //res.status(200).json({ song });
+});
+
 module.exports = router;
