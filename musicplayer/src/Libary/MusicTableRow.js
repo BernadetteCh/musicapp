aa000a860bb0e82fe4b0ff6b28526a20411719c8
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-22 22:35:07 +0100

    Add return in comment

--- musicplayer/src/Libary/MusicTableRow.js
+++ musicplayer/src/Libary/MusicTableRow.js
@@ -20,9 +20,47 @@ const MusicTableRow = ({
   const audioElem = useRef();
 
   const playMusic = (boolean) => {
+    // console.log("ID!!!!" + id);
+    // console.log("BOOLEEANN" + boolean);
+    // sameRender(boolean, playSong(id));
     sameRender(boolean, playSong(id));
   };
+  console.log("RETURN STATE" + state);
+  console.log("AUDIOICON RETURN" + audioIcon);
 
+  if (audioIcon == false && state == undefined) {
+    return (
+      <tr>
+        <td>
+          <img
+            src={PlayIcon}
+            className="play-icon"
+            onClick={() => playMusic(true, setAudioIcon(true))}
+
+            // onClick={() => setAudioIcon(true, playMusic(true))}
+          ></img>
+        </td>
+        <td
+          style={{
+            color: audioIcon == false && state == undefined ? "" : "",
+          }}
+        >
+          {title}
+        </td>
+        <td
+          style={{
+            color: audioIcon == true && state == undefined ? "" : "",
+          }}
+        >
+          {artist}
+        </td>
+        <td>{favoriteSong}</td>
+        <td>
+          <div>Remove from libary</div>
+        </td>
+      </tr>
+    );
+  }
   if (audioIcon == false && state == false) {
     console.log("HI from first statement");
     return (
@@ -38,14 +76,14 @@ const MusicTableRow = ({
         </td>
         <td
           style={{
-            color: audioIcon == true && state == false ? "green" : "",
+            color: audioIcon == false && state == true ? "green" : "",
           }}
         >
           {title}
         </td>
         <td
           style={{
-            color: audioIcon == true && state == false ? "green" : "",
+            color: audioIcon == true && state == true ? "green" : "",
           }}
         >
           {artist}
@@ -126,7 +164,7 @@ const MusicTableRow = ({
         </td>
       </tr>
     );
-  } else if (audioIcon == true) {
+  } else if (audioIcon == true && state == true) {
     return (
       <tr>
         <td>
@@ -135,7 +173,7 @@ const MusicTableRow = ({
               "https://i.gifer.com/origin/55/554818561cbf36d813ef2010cc9d66cc.gif"
             }
             className="pause-icon"
-            onClick={() => playMusic(false, setAudioIcon(true))}
+            onClick={() => playMusic(false, setAudioIcon(false))}
             //onClick={() => setMusic("", setAudioIcon(false), playMusic(false))}
           ></img>
         </td>
@@ -180,5 +218,197 @@ const MusicTableRow = ({
       </tr>
     );
   }
+
+  //--------------------ORIGINAL
+  // if (audioIcon == false && state == undefined) {
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={PlayIcon}
+  //           className="play-icon"
+  //           onClick={() => playMusic(true, setAudioIcon(true))}
+
+  //           // onClick={() => setAudioIcon(true, playMusic(true))}
+  //         ></img>
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == false && state == undefined ? "" : "",
+  //         }}
+  //       >
+  //         {title}
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == undefined ? "" : "",
+  //         }}
+  //       >
+  //         {artist}
+  //       </td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Remove from libary</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // }
+  // if (audioIcon == false && state == false) {
+  //   console.log("HI from first statement");
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={PlayIcon}
+  //           className="play-icon"
+  //           onClick={() => playMusic(true, setAudioIcon(true))}
+
+  //           // onClick={() => setAudioIcon(true, playMusic(true))}
+  //         ></img>
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == false && state == true ? "green" : "",
+  //         }}
+  //       >
+  //         {title}
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == true ? "green" : "",
+  //         }}
+  //       >
+  //         {artist}
+  //       </td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Remove from libary</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // } else if (
+  //   (state == false && audioIcon == false) ||
+  //   (state == false && audioIcon == true)
+  // ) {
+  //   console.log("Hi from second statement");
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={PlayIcon}
+  //           className="play-icon"
+  //           onClick={() => playMusic(true, setAudioIcon(true))}
+
+  //           // onClick={() =>
+  //           //   getData(id, setMusic, setAudioIcon(true), playMusic(true))
+  //           // }
+  //         ></img>
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == false ? "green" : "",
+  //         }}
+  //       >
+  //         {title}
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == false ? "green" : "",
+  //         }}
+  //       >
+  //         {artist}
+  //       </td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Remove from libary</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // } else if (state == undefined && audioIcon == true) {
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={PlayIcon}
+  //           className="play-icon"
+  //           // onClick={() => playMusic(true, setAudioIcon(true))}
+
+  //           onClick={() => setAudioIcon(true, playMusic(true))}
+  //         ></img>
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == false ? "green" : "",
+  //         }}
+  //       >
+  //         {title}
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == false ? "green" : "",
+  //         }}
+  //       >
+  //         {artist}
+  //       </td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Remove from libary</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // } else if (audioIcon == true && state == true) {
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={
+  //             "https://i.gifer.com/origin/55/554818561cbf36d813ef2010cc9d66cc.gif"
+  //           }
+  //           className="pause-icon"
+  //           onClick={() => playMusic(false, setAudioIcon(false))}
+  //           //onClick={() => setMusic("", setAudioIcon(false), playMusic(false))}
+  //         ></img>
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == true ? "green" : "",
+  //         }}
+  //       >
+  //         {title}
+  //       </td>
+  //       <td
+  //         style={{
+  //           color: audioIcon == true && state == true ? "green" : "",
+  //         }}
+  //       >
+  //         {artist}
+  //       </td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Not favorite song</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // } else if (audioIcon == false || (audioIcon == false && state == false)) {
+  //   return (
+  //     <tr>
+  //       <td>
+  //         <img
+  //           src={PlayIcon}
+  //           className="play-icon"
+  //           onClick={() => playMusic(true, setAudioIcon(true))}
+
+  //           // onClick={() => setAudioIcon(true, playMusic(true))}
+  //         ></img>
+  //       </td>
+  //       <td>{title}</td>
+  //       <td>{artist}</td>
+  //       <td>{favoriteSong}</td>
+  //       <td>
+  //         <div>Remove from libary</div>
+  //       </td>
+  //     </tr>
+  //   );
+  // }
 };
 export default MusicTableRow;
