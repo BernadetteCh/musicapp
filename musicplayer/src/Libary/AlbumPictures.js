173c60d02caf74c0aeb27deee777350d84c6b0b6
Bernadette Christiner <b.christiner93@gmail.com>
2023-01-26 22:53:50 +0100

    Add album pictures

--- musicplayer/src/Libary/AlbumPictures.js
+++ musicplayer/src/Libary/AlbumPictures.js
@@ -8,6 +8,22 @@ const AlbumPictures = () => {
         src="https://cdn.shoepping.at/medias/sys_master/cloud-images-1714-21211779/images/1714-21211779/h34/h11/13064392114206/vinyl-surface-sounds-kaleo-2-lp-analog-1617682957070-885.jpg"
         alt="kaleo-album-cover"
       ></img>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS475wv4naV0ttvTSwM0XgGsmWihLosIm_BGpNulGUw0C21paOT8c3ZuIMUj0mWKKwVKwI&usqp=CAU"
+        alt="metallica"
+      ></img>
+      <img
+        src="https://static.onecms.io/wp-content/uploads/sites/6/2021/09/09/foo-fighters-3.jpg"
+        alt="foofighters"
+      ></img>
+      <img
+        src="https://static.qobuz.com/images/covers/qa/n2/itn0l4jixn2qa_600.jpg"
+        alt="queen"
+      ></img>
+      <img
+        src="https://static.ra.co/images/profiles/lg/ash.jpg?dateUpdated=1594989985000"
+        alt="ash"
+      ></img>
     </div>
   );
 };
