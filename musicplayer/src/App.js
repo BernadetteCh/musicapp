ce022f90337143e926c9700d042bd492029fc0e2
Bernadette Christiner <b.christiner93@gmail.com>
2023-02-15 12:11:13 +0100

    Add classname

--- musicplayer/src/App.js
+++ musicplayer/src/App.js
@@ -42,7 +42,7 @@ function App() {
           <Col sm={3}>
             <Navigation />
           </Col>
-          <Col>
+          <Col className="home-section">
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route
