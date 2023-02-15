de757def601d476fd828f134c5fe4b0e95a24702
Bernadette Christiner <b.christiner93@gmail.com>
2023-02-15 12:12:18 +0100

    Remove style for h1

--- musicplayer/src/Home/Home.js
+++ musicplayer/src/Home/Home.js
@@ -4,9 +4,7 @@ import "../Home/Home.css";
 const Home = () => {
   return (
     <div>
-      <h1 className="header" style={{ marginBottom: "500px" }}>
-        Where words fail, music speaks. ...
-      </h1>
+      <h1 className="header">Where words fail, music speaks. ...</h1>
     </div>
   );
 };
