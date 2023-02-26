dfdf139168dc4f738eb054be3dedf37ea907c28d
Bernadette Christiner <b.christiner93@gmail.com>
2023-02-26 17:06:20 +0100

    Create conditional rendereing to play a song

--- musicplayer/src/Libary/MusicTableRow.js
+++ musicplayer/src/Libary/MusicTableRow.js
@@ -11,7 +11,7 @@ const MusicTableRow = ({
   sameRender,
   state,
   id,
-
+  key,
   index,
   file,
   myKey,
@@ -21,7 +21,7 @@ const MusicTableRow = ({
   const [audioIcon, setAudioIcon] = useState([
     { audioIcon: "no music yet", id: id, index: index, isActive: false },
   ]);
-  const [clickedTableRow, setClickedTableRow] = useState(null);
+  const [selectedRow, setSelectedRow] = useState(null);
 
   useEffect(() => {
     if (audioIcon[0].audioIcon !== "no music yet") {
@@ -36,49 +36,19 @@ const MusicTableRow = ({
     }
   }, []);
 
-  // useEffect(() => {
-  //   if (state == true && audioIcon[0].audioIcon !== "no music yet") {
-  //     const audioIcon = [
-  //       { audioIcon: true, id: id, index: index, isActive: true },
-  //     ];
-  //     window.localStorage.setItem("icon", JSON.stringify(audioIcon));
-  //     // setAudioIcon({ audioIcon: true, id: id, index: index });
-  //   }
-  //   if (state == false && audioIcon[0].audioIcon !== "no music yet") {
-  //     // setAudioIcon({ audioIcon: false, id: id, index: index });
-  //     const audioIcon = [
-  //       { audioIcon: false, id: id, index: index, isActive: false },
-  //     ];
-  //     window.localStorage.setItem("icon", JSON.stringify(audioIcon));
-  //   }
-  // }, [state]);
-
   const playMusic = (boolean) => {
     sameRender(boolean, playSong(id));
   };
 
-  // console.log(audioIcon);
-  // console.log("STATE * " + state);
-  //wenn man auf die Page kommt und no nix im localstorage ist
-  const clickOnTableRow = (index) => {
-    if (clickOnTableRow !== index) {
-      // handle if user clicks again the same row
-      setClickedTableRow(index);
-    } else {
-      setClickedTableRow(null); // set clicked row to null if same row is selected
-    }
-  };
-
   if (state === undefined && audioIcon[0].audioIcon == "no music yet") {
-    // console.log("FIRST CONDITION");
     return (
-      <tr>
+      <tr className={selectedRow === index ? "selected" : "notselected"}>
         <td
           onClick={() => {
             setAudioIcon(
               [{ audioIcon: true, id: id, index: index, isActive: true }],
               playMusic(true),
-              setClickedTableRow(index)
+              setSelectedRow(index)
             );
           }}
         >
@@ -95,349 +65,241 @@ const MusicTableRow = ({
       </tr>
     );
     //CONDITIONS START WHEN I Click song next to each other
-  } else if (state == true) {
+  } else if (audioIcon[0].index === index && state === true) {
     return (
       <tr>
-        <td
-          onClick={() => {
-            setAudioIcon(
-              [{ audioIcon: true, id: id, index: index, isActive: true }],
-              playMusic(false),
-              setClickedTableRow(index)
-            );
-          }}
-        >
-          HI
-        </td>
+        {state === true &&
+        audioIcon[0].id == id &&
+        audioIcon[0].isActive == true &&
+        audioIcon[0].audioIcon == true ? (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: false, id: id, index: index, isActive: false }],
+                  playMusic(false),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        ) : (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: true, id: id, index: index, isActive: true }],
+                  playMusic(true),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        )}
       </tr>
     );
-  } else if (state == false) {
+  } else if (audioIcon[0].index === index && state === false) {
     return (
       <tr>
-        <td
-          onClick={() => {
-            setAudioIcon(
-              [{ audioIcon: false, id: id, index: index, isActive: true }],
-              playMusic(true),
-              setClickedTableRow(index)
-            );
-          }}
-        >
-          Berni
-        </td>
+        {state === true &&
+        audioIcon[0].id == id &&
+        audioIcon[0].isActive == true &&
+        audioIcon[0].audioIcon == true ? (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: false, id: id, index: index, isActive: false }],
+                  playMusic(false),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        ) : (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: true, id: id, index: index, isActive: true }],
+                  playMusic(true),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        )}
+      </tr>
+    );
+  } else if (state === true) {
+    return (
+      <tr>
+        {state === true &&
+        audioIcon[0].id == id &&
+        audioIcon[0].isActive == true &&
+        audioIcon[0].audioIcon == true ? (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [
+                    {
+                      audioIcon: false,
+                      id: id,
+                      index: index,
+                      isActive: false,
+                    },
+                  ],
+                  playMusic(false),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        ) : (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: true, id: id, index: index, isActive: true }],
+                  playMusic(true),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        )}
+      </tr>
+    );
+  } else if (state === false) {
+    return (
+      <tr>
+        {state === true &&
+        audioIcon[0].id == id &&
+        audioIcon[0].isActive == true &&
+        audioIcon[0].audioIcon == true ? (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [
+                    {
+                      audioIcon: false,
+                      id: id,
+                      index: index,
+                      isActive: false,
+                    },
+                  ],
+                  playMusic(false),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        ) : (
+          <div className={selectedRow === index ? "selected" : "notselected"}>
+            <td
+              onClick={() => {
+                setAudioIcon(
+                  [{ audioIcon: true, id: id, index: index, isActive: true }],
+                  playMusic(true),
+                  setSelectedRow(index)
+                );
+              }}
+            >
+              #{index + 1}.
+            </td>
+            <td></td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{favoriteSong}</td>
+            <td>Icon for duration</td>
+
+            <td>
+              <div>Remove from libary</div>
+            </td>
+          </div>
+        )}
       </tr>
     );
   }
 };
 
 export default MusicTableRow;
-//THis version workds a littel bit
-
-//if (state === undefined && audioIcon[0].audioIcon == "no music yet") {
-//   // console.log("FIRST CONDITION");
-//   return (
-//     <div>
-//       <tr>
-//         <td
-//           onClick={() => {
-//             setAudioIcon(
-//               [{ audioIcon: true, id: id, index: index }],
-//               playMusic(true),
-//               setClickedTableRow(index),
-//               setActiveSong(true)
-//             );
-//           }}
-//         >
-//           #{index + 1}.
-//         </td>
-//         <td></td>
-//         <td>{title}</td>
-//         <td>{artist}</td>
-//         <td>{favoriteSong}</td>
-//         <td>Icon for duration</td>
-//         <td>
-//           <div>Remove from libary</div>
-//         </td>
-//       </tr>
-//     </div>
-//   );
-//   //CONDITIONS START WHEN I Click song next to each other
-// } else if (audioIcon[0].index === index && state === true) {
-//   // console.log("SECOND CONDITION");
-//   // console.log("ACTIVESONG" + activeSong);
-
-//   return (
-//     <tr>
-//       {state === true &&
-//       audioIcon[0].index == index &&
-//       activeSong == true &&
-//       audioIcon[0].audioIcon == true ? (
-//         <div>
-//           <td
-//             style={{
-//               color:
-//                 audioIcon[0].id === id && audioIcon[0].audioIcon == true
-//                   ? "yellow"
-//                   : "blue",
-//             }}
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: false, id: id, index: index }],
-//                 playMusic(false),
-//                 setClickedTableRow(index),
-//                 setActiveSong(false)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       ) : (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       )}
-//     </tr>
-//   );
-// } else if (audioIcon[0].index === index && state === false) {
-//   // console.log("THIRD CONDITION");
-//   return (
-//     <tr>
-//       {audioIcon[0].audioIcon == false &&
-//       state == false &&
-//       activeSong == false &&
-//       audioIcon[0].index === index ? (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       ) : (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       )}
-//     </tr>
-//   );
-// }
-//THis is without pausing
-// if (state === undefined && audioIcon[0].audioIcon == "no music yet") {
-//   console.log("FIRST CONDITION");
-//   return (
-//     <div>
-//       <tr>
-//         <td
-//           onClick={() => {
-//             setAudioIcon(
-//               [{ audioIcon: true, id: id, index: index }],
-//               playMusic(true),
-//               setClickedTableRow(index),
-//               setActiveSong(true)
-//             );
-//           }}
-//         >
-//           #{index + 1}.
-//         </td>
-//         <td></td>
-//         <td>{title}</td>
-//         <td>{artist}</td>
-//         <td>{favoriteSong}</td>
-//         <td>Icon for duration</td>
-//         <td>
-//           <div>Remove from libary</div>
-//         </td>
-//       </tr>
-//     </div>
-//   );
-//   //CONDITIONS START WHEN I Click song next to each other
-// } else if (audioIcon[0].index === index && state === true) {
-//   console.log("SECOND CONDITION");
-//   console.log("ACTIVESONG" + activeSong);
-//   return (
-//     <tr>
-//       {state === true &&
-//       clickedTableRow == index &&
-//       audioIcon[0].audioIcon == true &&
-//       activeSong == false ? (
-//         <div>
-//           <td
-//             style={{
-//               color:
-//                 clickedTableRow == index && audioIcon[0].audioIcon == true
-//                   ? "yellow"
-//                   : "blue",
-//             }}
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: false, id: id, index: index }],
-//                 playMusic(false),
-//                 setClickedTableRow(index),
-//                 setActiveSong(false)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       ) : (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       )}
-//     </tr>
-//   );
-// } else if (audioIcon[0].index === index && state === true) {
-//   console.log("THIRD CONDITION");
-//   console.log("ACTIVESONG" + activeSong);
-//   return (
-//     <tr>
-//       {audioIcon[0].audioIcon == false &&
-//       state == false &&
-//       activeSong == false ? (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       ) : (
-//         <div>
-//           <td
-//             onClick={() => {
-//               setAudioIcon(
-//                 [{ audioIcon: true, id: id, index: index }],
-//                 playMusic(true),
-//                 setClickedTableRow(index),
-//                 setActiveSong(true)
-//               );
-//             }}
-//           >
-//             #{index + 1}.
-//           </td>
-//           <td></td>
-//           <td>{title}</td>
-//           <td>{artist}</td>
-//           <td>{favoriteSong}</td>
-//           <td>Icon for duration</td>
-
-//           <td>
-//             <div>Remove from libary</div>
-//           </td>
-//         </div>
-//       )}
-//     </tr>
-//   );
-// }
